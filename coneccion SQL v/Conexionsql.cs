f76f6495a731d91888a90d08d4847b41fc6aa513
ClaudiaIngridTudisco <ingridtudisco@gmail.com>
2023-06-04 11:22:24 -0300

    conexión SQL y creación de usuarios

---
+++ coneccion SQL v/Conexionsql.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace coneccion_SQL_v
+{
+    public class Conexionsql
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public Conexionsql()
+        {
+            try
+            {
+                cn = new SqlConnection("Data Source=.;Initial Catalog=bondAPPetite;Integrated Security=True");
+                cn.Open();
+                MessageBox.Show("Conectado");
+
+            }
+            catch(Exception ex) 
+            {
+            MessageBox.Show("No se conectó con la base de datos: " + ex.ToString());
+            }
+        }
+
+
+        
+        public int roll_id = 1;
+        public DateTime fechaDeRegistro = DateTime.Now;
+        public Byte Activo = 1;
+
+        public string Insertar(string nombre, string email, string password, int rol_id, DateTime fechaDeRegistro, Byte Activo,  string telefono, string dni)
+        {
+            
+            string salida = "Usuario creado correctamente";
+            try
+            {
+                cmd = new SqlCommand("Insert into Usuario(nombre,email,password,rol_id,fechaDeRegistro,Activo,telefono,dni) values('" + nombre + "','" + email + "','" + password + "', "+rol_id+",'" + fechaDeRegistro + "',"+Activo+",'" + telefono + "','" + dni + "')", cn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                salida = "No se creó el usuario: " + ex.ToString();
+            }
+            return salida;
+        }
+
+        public int UsuarioRegistrado(int dni)
+        {
+            int contador = 0;
+            try
+            {
+                cmd = new SqlCommand("Select * from Usuario where dni='" + dni + "'", cn);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    contador++;
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo realizar la consulta: " + ex.ToString());
+            }
+
+            return contador;
+        }
+    }
+}
---
+++ coneccion SQL v/Login.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace coneccion_SQL_v
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label1.Location = new System.Drawing.Point(63, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 634);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
